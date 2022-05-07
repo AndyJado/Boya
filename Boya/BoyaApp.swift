b4e67d67f4367a67c8b7567737b7a17015c48887
AndyJado <101876416+AndyJado@users.noreply.github.com>
2022-05-07 15:38:54 +0800

    Initial Commit

---
+++ Boya/BoyaApp.swift
@@ -0,0 +1,20 @@
+//
+//  BoyaApp.swift
+//  Boya
+//
+//  Created by Andy Jado on 2022/5/7.
+//
+
+import SwiftUI
+
+@main
+struct BoyaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
